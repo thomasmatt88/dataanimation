23e97955c26628f162dc97b3b5a14375c733f9b1
Matthew Thomas <matthewthomas@Matthews-MacBook-Air.local>
2019-07-16 16:58:18 -0700

    Tab2 now includes End Time label and entry that updates based off of video file openend

--- dataanimation.py
+++ dataanimation.py
@@ -9,7 +9,7 @@ from tkinter import messagebox
 from tkinter.filedialog import askopenfilename
 
 # custom modules
-from videotimestamp import videotimestamp
+from videotimestamp import videotimestamp, videoendtime
 from videotrim import trim_start
 
 def main():
@@ -24,6 +24,7 @@ class Tab2(ttk.Frame):
         # --------------- data overlay attributes ------------------------------
         self.video_file_path = ""
         self.time_stamp = tk.StringVar(value = "%Y-%M-%D %H:%M:%S")
+        self.time_stamp_end = tk.StringVar(value = "%Y-%M-%D %H:%M:%S")
 
         # --------------- data overlay frame -----------------------------------
         self.videofileButton = tk.Button(self, text = "Open Video File", \
@@ -36,13 +37,21 @@ class Tab2(ttk.Frame):
                                                              self.video_file_path))
         self.trimvideoButton.pack(side = tk.LEFT)       
         
-        self.t_stamp_label = tk.Label(self, text = "Time-Stamp: ", fg = "blue")
-        self.t_stamp_label.pack(side = tk.LEFT)
+        self.st_stamp_label = tk.Label(self, text = "Start-Time: ", fg = "blue")
+        self.st_stamp_label.pack(side = tk.LEFT)
 
         #self.time_stamp = tk.StringVar(value = "%Y-%M-%D %H:%M:%S")
-        self.time_stamp_entry = tk.Entry(self, \
+        self.stime_stamp_entry = tk.Entry(self, \
                                         textvariable = self.time_stamp)
-        self.time_stamp_entry.pack(side = tk.LEFT)
+        self.stime_stamp_entry.pack(side = tk.LEFT)
+
+        self.et_stamp_label = tk.Label(self, text = "End-Time: ", fg = "blue")
+        self.et_stamp_label.pack(side = tk.LEFT)
+
+        #self.time_stamp = tk.StringVar(value = "%Y-%M-%D %H:%M:%S")
+        self.etime_stamp_entry = tk.Entry(self, \
+                                        textvariable = self.time_stamp_end)
+        self.etime_stamp_entry.pack(side = tk.LEFT)     
 
     # tab2 helper -----------------------------------------------------------------------
     def videofileopen(self):
@@ -50,6 +59,7 @@ class Tab2(ttk.Frame):
             v = askopenfilename()
             # use videotimestamp method from videotimestamp module
             self.time_stamp.set(str(videotimestamp(v))) #videotimestamp returns datetime.datetime object
+            self.time_stamp_end.set(str(videoendtime(v)))
             #self.time_stamp_entry.delete(0, tk.END)
             #self.time_stamp_entry.insert(0, self.time_stamp)
             self.video_file_path = v
--- videotimestamp.py
+++ videotimestamp.py
@@ -2,15 +2,11 @@ from hachoir.parser import createParser
 from hachoir.metadata import extractMetadata
 import re
 from datetime import datetime
+from datetime import timedelta
 
 def videotimestamp(video_file_path):
 
-    #extract metadata from video file
-    parser = createParser(video_file_path)
-    metadata = extractMetadata(parser)
-
-    #extract creation date from metadata
-    metadata_string = str(metadata)
+    metadata_string = metadata(video_file_path)
     start = "Creation date: "
     end = "\n"
     time_string = re.search('%s(.*)%s'%(start, end), metadata_string).group(1)
@@ -19,3 +15,58 @@ def videotimestamp(video_file_path):
 
     return video_creation_datetime
 
+def videoendtime(video_file_path):
+
+    start_time = videotimestamp(video_file_path)
+    duration = duration_seconds(video_file_path)
+
+    end_time = start_time + timedelta(seconds = duration)
+    
+    return end_time
+
+def metadata(video_file_path):
+    "returns a string of metadata from given video"
+
+    #extract metadata from video file
+    parser = createParser(video_file_path)
+    metadata = extractMetadata(parser)
+
+    #extract creation date from metadata
+    metadata_string = str(metadata)
+
+    return metadata_string
+
+# helper functions -------------------------------------------------------------------------
+
+def duration_string(video_file_path):
+
+    metadata_string = metadata(video_file_path)
+    start = "Duration: "
+    end = "\n"
+    duration_string = re.search('%s(.*)%s'%(start, end), metadata_string).group(1)
+
+    return duration_string
+
+def duration_seconds(video_file_path):
+
+    x = duration_string(video_file_path)
+
+    start_min = ""
+    end_min = " min"
+
+    try:
+        minutes = int(re.search('%s(.*)%s'%(start_min, end_min), x).group(1))
+    except:
+        minutes = 0
+
+    try:
+        start_sec = "min "
+        end_sec = " sec"
+        seconds = int(re.search('%s(.*)%s'%(start_sec, end_sec), x).group(1))
+    except:
+        start_sec = ""
+        end_sec = " sec"
+        seconds = int(re.search('%s(.*)%s'%(start_sec, end_sec), x).group(1))
+
+    return int(minutes*60 + seconds)
+    
